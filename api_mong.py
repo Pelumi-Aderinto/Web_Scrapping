7e598b1c356e2d06e46b4fecb3dbd06d036146a5
Pelumi-Aderinto <aderintopelumi@gmail.com>
2022-08-25 15:03:12 +0100

    First Test

---
+++ api_mong.py
@@ -0,0 +1,54 @@
+from flask import Flask , make_response , request , jsonify
+from flask_mongoengine import MongoEngine
+import collections
+from dotenv import load_dotenv, find_dotenv
+import os
+
+app = Flask(__name__)
+load_dotenv(find_dotenv())
+password = os.environ.get("MONGODB")
+conn_str = f"mongodb+srv://pelvic:{password}@cluster0.zst9xg2.mongodb.net/Online_News?retryWrites=true&w=majority"
+app.config['MONGODB_HOST'] = conn_str
+
+db = MongoEngine()
+db.init_app(app)
+
+class News(db.Document):
+    Headlines = db.StringField()
+    Links = db.StringField()
+
+    def to_json(self):
+        return {
+            'Headlines' : self.Headlines,
+            'Links' : self.Links
+
+        }
+
+
+@app.route('/Online_News/News', methods=['GET','POST'])
+def api_search():
+    if request.method == 'GET':
+        print(request.method)
+        D_News = []
+        for news in News.objects:
+            D_News.append(news)
+        return make_response(jsonify(D_News), 200)
+    elif request.method == 'POST':
+        content = request.json
+        news = News(Headlines = content['Headlines'], Links = content['Links'])
+        news.save()
+        return make_response('', 201)
+
+
+    # if D_News:
+    #     return make_response(jsonify(D_News.to_json(), 200))
+    # else:
+    #     return make_response('', 404)
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
